c693d4f0cfe37fedbb7be50d61382f32e3eb160e
Svett Ralchev <svet@ralch.com>
2016-11-15 23:15:11 +0000

    Produce a git repository for blog

--- blog/generate.bash
+++ blog/generate.bash
@@ -2,8 +2,29 @@
 
 [ -z "$DEBUG" ] || set -x
 
+main() {
+  git_configure
+  git_init_destination
+  git_commit_changes
+}
+
+git_configure() {
+  git config --global user.email "nobody@concourse.ci"
+  git config --global user.name "Concourse"
+}
+
+git_init_destination() {
+  cd blog-content
+  git init
+}
+
+git_commit_changes() {
+  git add .
+  git commit -m "Generated blog content for $BASE_URL"
+}
+
 generate_blog() {
   hugo --source blog-source --destination blog-content --config "$BLOG_CONFIG" --theme "$THEME_NAME" --baseURL "$BASE_URL"
 }
 
-generate_blog
+main
