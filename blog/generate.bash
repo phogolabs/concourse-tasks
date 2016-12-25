3eb263829e760a00be74fb4db41cbb7752d36087
Svett Ralchev <svet@ralch.com>
2016-12-25 11:03:03 +0000

    Add heroku tasks

--- blog/generate.bash
+++ blog/generate.bash
@@ -17,14 +17,21 @@ git_configure() {
 }
 
 git_commit_changes() {
+  local commit_message
+  commit_message="Publish a new blog post from $(commit_sha)"
+
   cd "$DESTINATION"
   git init
   git add .
-  git commit --no-verify -m "Publishing new version of the blog"
+  git commit --no-verify -m "$commit_message"
 }
 
 generate_blog() {
   hugo --source blog-source --destination "$DESTINATION" --config "$BLOG_CONFIG" --theme "$THEME_NAME" --baseURL "$BASE_URL"
+}
+
+commit_sha() {
+  cd blog-source && git rev-parse HEAD
 }
 
 main
